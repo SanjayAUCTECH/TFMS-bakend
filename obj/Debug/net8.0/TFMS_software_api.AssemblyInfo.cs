366ea31a3d0cd90490af65cfd686c4054162b647
sanjaykumar <sanjay.kumar@auctech.in>
2026-07-14 12:19:00 +0530

    done data delete

--- obj/Debug/net8.0/TFMS_software_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/TFMS_software_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+996c721ee37b1d125d9536776e29901caa5b6b86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6584d876e3c4eeece40cff2d65222d2be5edc118")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
