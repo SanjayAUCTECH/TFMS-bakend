f28f67b9354ed8484ede8a576b1184756831dc75
sanjaykumar <sanjay.kumar@auctech.in>
2026-09-05 18:23:40 +0530

    publice121211111

--- obj/Debug/net8.0/TFMS_software_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/TFMS_software_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1de0852846dd8da95dddf9d3f015f13f8b2ef544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e59478103ff3771bcd7df8741d85c1686d8e8f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Release/net8.0/TFMS_software_api.AssemblyInfo.cs
+++ obj/Release/net8.0/TFMS_software_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef1dcfd9b87463bfaa64211a4481ebd6831acb45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e59478103ff3771bcd7df8741d85c1686d8e8f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
