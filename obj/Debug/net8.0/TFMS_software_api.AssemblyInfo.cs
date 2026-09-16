76674bd96ee131a54dc2c68f8ef99876d58338d9
sanjaykumar <sanjay.kumar@auctech.in>
2026-09-16 10:06:05 +0530

    1111111111

--- obj/Debug/net8.0/TFMS_software_api.AssemblyInfo.cs
+++ obj/Debug/net8.0/TFMS_software_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d6f20c5720e4ddfccfc4c24a2370743e824ca4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8620157aa1e90afad07e8ebe5bec7bc4e79331c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
