ab9fb822908bea2251a4733632ccd2fbf2f7c834
sanjaykumar <sanjay.kumar@auctech.in>
2026-09-19 18:44:06 +0530

    111111111111

--- obj/Release/net8.0/TFMS_software_api.AssemblyInfo.cs
+++ obj/Release/net8.0/TFMS_software_api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b3330bad35106f7e979e76590197028cc6b57e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+734913babf2bc3fc1a701fe6b2d3b5dddead5545")]
 [assembly: System.Reflection.AssemblyProductAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TFMS_software_api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
